3dd589063742329a1341dd8f55ed46ce3cf1122a
Matilde | Software Engineer studying data area <Eduarda.lem049@outlook.com>
2026-01-16 10:49:12 -0300

    Update solution - CodeHub

--- python/8-kyu/area-or-perimeter/area-or-perimeter.py
+++ python/8-kyu/area-or-perimeter/area-or-perimeter.py
@@ -10,8 +10,4 @@ def area_or_perimeter(l: int, w: int) -> int | None:
         l * w (int): If it is a square, return its area. 
         (2 * l) + (2 * w) (int): If it is a rectangle, return its perimeter.
     """
-    
-    if l == w:
-        return l * w
-    else:
-        return (2 * l) + (2 * w)
+    return l * w if l == w else (l + w) * 2
