c136c96a595963aabcec709e43d0e458ae02073f
matilde <Eduarda.lem049@outlook.com>
2026-01-23 16:54:22 -0300

    Update solution - CodeHub

--- python/7-kyu/vowel-count/vowel-count.py
+++ python/7-kyu/vowel-count/vowel-count.py
@@ -1,2 +1,3 @@
-def get_count(sentence):
+def get_count(sentence: str) -> int:
+    "Receives a phrase or sentence and returns the number of vowels it contains."
     return sum(1 for letter in sentence.lower() if letter in "aeiou")
