b2498db560d34d58c4fd2dc7eca232eeedd2c68c
Maria Eduarda  [ MEL ] <Eduarda.lem049@outlook.com>
2026-02-09 17:55:35 -0300

    Update solution - CodeHub

--- python/7-kyu/categorize-new-member/categorize-new-member.py
+++ python/7-kyu/categorize-new-member/categorize-new-member.py
@@ -1,2 +1,2 @@
-def open_or_senior(data):
-    return  ["Senior" if member[0]>= 55 and member[1] > 7 else "Open" for member in data]
+def open_or_senior(data: list[list]) -> list:
+    return ["Senior" if member[0] >= 55 and member[1] > 7 else "Open" for member in data]
