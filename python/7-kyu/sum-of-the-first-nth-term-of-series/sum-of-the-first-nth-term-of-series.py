7207e38439e64b439cb9906654d632679dd0faa1
Maria Eduarda  [ MEL ] <Eduarda.lem049@outlook.com>
2026-02-25 23:15:03 -0300

    Update solution - CodeHub

--- python/7-kyu/sum-of-the-first-nth-term-of-series/sum-of-the-first-nth-term-of-series.py
+++ python/7-kyu/sum-of-the-first-nth-term-of-series/sum-of-the-first-nth-term-of-series.py
@@ -1,8 +1,3 @@
 def series_sum(n):
-    result = 1
-    denominador = 1
-    for i in range(1, n):
-        denominador += 3
-        result += (1 / (denominador)) 
+    return f"{sum(1 / i for i in range(1, n * 3, 3)):.2f}" if n > 0 else "0.00"
     
-    return f"{result:.2f}" if n > 0 else "0.00"
